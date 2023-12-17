1081a2a737d9a630c689b00f2940f032733d4147
Hgc88 <99982757+hgcsasdas@users.noreply.github.com>
2023-12-17 09:53:57 +0100

    Update comandos_locales.py
    quitado las rutas

--- functions/comandos_locales.py
+++ functions/comandos_locales.py
@@ -3,9 +3,9 @@ import subprocess as sp
 
 paths = {
     'notepad': "C:\\Windows\\System32\\notepad.exe",
-    'discord': "C:\\Users\\carls\\AppData\\Local\\Discord\\app-1.0.9008\\Discord.exe",
+    'discord': "Path",
     'calculator': "C:\\Windows\\System32\\calc.exe",
-    'lol': "C:\\Riot Games\\League of Legends\\LeagueClient.exe"
+    'lol': "path"
 }
 
 
